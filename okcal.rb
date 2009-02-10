80e154f04730725c25f64217f2d59465db4a7c7c
grant <grant@reki.local>
2009-02-10 15:43:03 -0600

    playing with stuff, added readme

--- okcal.rb
+++ okcal.rb
@@ -1,33 +1,5 @@
-# Calendar code for 
-
-## irb example
-#require 'okcal'
-#cal = OKCal.new
-#cal.new_event('daily','test1','20090215')
-#cal.new_event('monthly','test5','20090215',{'stop' => '2009-12-12', 'frequency' => 0})
-#
-#
-## End irb exampple
-
-##Here is a script that uses the code
-# #!/usr/bin/env ruby
-# require 'rubygems'
-# require 'okcal'
-# 
-# cal = OKCal.new
-# cal.new_event('daily','test1','20090215')
-# cal.new_event('daily','test1','20090215', {'stop' => '2009-03-18'})
-# cal.new_event('weekly','test3','20090215',{'stop' => '2009-03-18', 'frequency' => 3})
-# cal.new_event('weekly','test4','20090215',{'stop' => '2009-03-18', 'frequency' => ['Monday','Thursday']})
-# cal.new_event('monthly','test5','20090215',{'stop' => '2009-12-12', 'frequency' => 0})
-# cal.new_event('monthly','test6','2009-12-18',{'stop' => '2010-12-18', 'frequency' => 'last', 'day' => 'Tuesday'})
-# cal.new_event('yearly','test7','2009-12-18',{'stop' => '2018-12-18'})
-# cal.new_event('yearly','test8','2009-12-18')
-# # To get a date do: cal.get_events(<date>)
-# events = cal.get_events('2009-02-16')
-print events
-
-
+#get libs we need
+require 'rubygems'
 #class to make events and get them out
 class OKCal 
   attr_reader :cal,:events, :error
--- okcal_run.rb
+++ okcal_run.rb
@@ -1,10 +1,9 @@
 #!/usr/bin/env ruby
-require 'rubygems'
 require 'okcal.rb'
 
 cal = OKCal.new
 cal.new_event('daily','test1','20090215')
-cal.new_event('daily','test1','20090215', {'stop' => '2009-03-18'})
+cal.new_event('daily','test2','20090215', {'stop' => '2009-03-18'})
 cal.new_event('weekly','test3','20090215',{'stop' => '2009-03-18', 'frequency' => 3})
 cal.new_event('weekly','test4','20090215',{'stop' => '2009-03-18', 'frequency' => ['Monday','Thursday']})
 cal.new_event('monthly','test5','20090215',{'stop' => '2009-12-12', 'frequency' => 0})
